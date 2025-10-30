a7c52e4413860680c1a9c2372d39ed0cd6cfb9af
DuongLeThuong <dtc235340030@ictu.edu.vn>
2025-10-30 12:46:37 +0700

    demo2

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -2,10 +2,9 @@ package demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Demo SpotBugs CI running...");
-
         String role = getUserRole();
-        if ("admin".equals(role)) {
+
+        if (role == "admin") {
             System.out.println("Full access granted");
         } else {
             System.out.println("Limited access");
@@ -13,6 +12,6 @@ public class Main {
     }
 
     public static String getUserRole() {
-        return "admin";
+        return new String("admin");
     }
 }
